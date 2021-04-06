eb57df13d1cf6fc7ed77858dcf7b176475373d7f
MANVENDRA SINGH <Manvendra Singh Songara>
2021-04-06 23:46:40 +0530

    fun

---
+++ exp_fun.js
@@ -0,0 +1,11 @@
+//function defination
+function hello(n){
+    return `hello${n}`;
+}
+/*myFun=(x)=>{
+    return `hello ${x} good morning...`;
+}*/
+//obj.pro
+//module.exports={};
+module.exports=hello;
+module.exports=myFun;
---
+++ imp_fun.js
@@ -0,0 +1,5 @@
+let fun = require('./exp_fun');
+l//et fun2 = require('./exp_fun');
+console.log(fun);
+var b= fun("antimsongara");
+console.log(b);
